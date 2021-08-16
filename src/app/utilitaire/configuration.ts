3ff76e8d6a4c0e027d205f3f12229054712614a4
Razakason Jessi <razakasonjessi@gmail.com>
2021-08-16 20:10:17 +0300

    debug prod

--- src/app/utilitaire/configuration.ts
+++ src/app/utilitaire/configuration.ts
@@ -1,10 +1,10 @@
 export const config = {
     "baseOracle": {
-        "prod" : "https://mbdsp7-spring-tpt-pari.herokuapp.com/",
+        "prod" : "https://mbdsp7-spring-tpt-pari.herokuapp.com",
         "local": "http://localhost:8080"
     },
     "baseApi" : {
-        "prod" : "https://mbdsp7-node-tpt-pari.herokuapp.com/",
+        "prod" : "https://mbdsp7-node-tpt-pari.herokuapp.com",
         "local": "http://localhost:3000"
     },
     "localStorage" : {
