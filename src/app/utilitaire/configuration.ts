6a3f7ad05e27d0bdc19763ab0feb0784e34bae38
Razakason Jessi <razakasonjessi@gmail.com>
2021-08-16 20:02:22 +0300

    deploy on prod

--- src/app/utilitaire/configuration.ts
+++ src/app/utilitaire/configuration.ts
@@ -1,14 +1,14 @@
 export const config = {
     "baseOracle": {
-        "prod" : "https://backmbdsmevajessi.herokuapp.com",
+        "prod" : "https://mbdsp7-spring-tpt-pari.herokuapp.com/",
         "local": "http://localhost:8080"
     },
     "baseApi" : {
-        "prod" : "https://backmbdsmevajessi.herokuapp.com",
+        "prod" : "https://mbdsp7-node-tpt-pari.herokuapp.com/",
         "local": "http://localhost:3000"
     },
     "localStorage" : {
         "user" : "user-key"
     },
-    "envDefault" : "local"
+    "envDefault" : "prod"
 }
